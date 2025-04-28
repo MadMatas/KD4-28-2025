520c869b13c8b666236ade76a8d9410182395590
Moksleivis <Moksleivis@kitm.local>
2025-04-28 15:16:11 +0300

    Isleisk

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Minecraft from './components/Minecraft'
+import { useState } from 'react'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Minecraft />
+    </>
+  )
+}
+
+export default App
